04738a117f08668ca1e7d8537f8376f1a553b4f7
Roberto Payan <kirindu3@gmail.com>
2025-01-08 10:54:13 -0700

    test

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
    // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: 'http://localhost:4000/api'
+   // baseURL: 'http://localhost:4000/api'
+      baseURL: 'https://backend-rpi-shop-computer-nodejs.onrender.com/api'
 });
 
 export default api;
